df2fb325cbd16517c05fe518415811f4464065c4
richjjoh <71095198+richjjoh@users.noreply.github.com>
2023-11-06 12:57:24 +0000

    Create main.tf
    Raw creation

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "tado" {
+  username = "var.TADO_PASSWORD"
+  password = "var.TADO_USERNAME"
+version = 1.0.6
+}
+
+
+resource "tado_heating_schedule" "kitchen" {
+  home_name = "My Home"
+  zone_name = "Kitchen"
+
+  mon_sun = [
+    { heating = false, start = "00:00", end = "06:00" },
+    { heating = true, temperature = 20.0, start = "06:00", end = "21:00" },
+    { heating = false, start = "21:00", end = "00:00" },
+  ]
+}
+
